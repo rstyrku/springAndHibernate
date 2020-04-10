52d7bf0176c1080617fbc799fbc044832d978ce0
Roman  Styrku <roman.styrku@Romans-MacBook-Pro.local>
2020-04-09 21:18:44 -0400

    Through section 35

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,7 +11,10 @@ public class MyDemoLoggingAspect {
 	// this is where we add all of our related advices for logging 
 	
 	// let's start with an @Before advice
-	@Before("execution(public void addAccount())")
+	
+	//@Before("execution(public void add*())")
+	
+	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
 	}
