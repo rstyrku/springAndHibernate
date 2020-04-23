292c631b3753e42992dfede5ae79743a46cb7343
Roman  Styrku <roman.styrku@Romans-MacBook-Pro.local>
2020-04-23 14:32:56 -0400

    All Through the end

---
+++ 32-thymeleafdemo-employees-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springboot.thymeleafdemo.model.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+	
+	private List<Employee> theEmployees;
+	
+	@PostConstruct
+	private void loadData() {
+		Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslie@luv2code.com");
+		Employee emp2 = new Employee(2, "Emma", "Baumgarten", "emma@luv2code.com");
+		Employee emp3 = new Employee(1, "Avani", "Gupta", "leslie@luv2code.com");
+	
+		theEmployees = new ArrayList<>();
+		theEmployees.add(emp1);
+		theEmployees.add(emp2);
+		theEmployees.add(emp3);
+	}
+	
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		theModel.addAttribute("employees", theEmployees);
+		return "list-employees";
+	}
+
+}
